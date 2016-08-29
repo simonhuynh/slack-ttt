448bfdd1f147a83d475427a1380daf147f17dff8
Simon Huynh <simonhuynh@gmail.com>
2016-08-28 21:14:43 -0700

    preliminary changes for help command

---
+++ constants.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ Constants 
+*/
+
+# COMMANDS
+define("VS_COMMAND", "vs");
+define("HELP", "help");
+
+
+# PROMPTS
+define("NEW_GAME_PROMPT", "\xA\xAYou can start a new game by typing: /ttt vs [Slack user]");
+define("PLAYER_TURN_INSTRUCTIONS", "\xA\xAWhen it's your turn make your mark with: /ttt [position # from 1 to 9]");
+?>
--- require.php
+++ require.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'constants.php';
 require_once 'gateway/db.php';
 require_once 'gateway/slack_config.php';
 require_once 'models.php';
